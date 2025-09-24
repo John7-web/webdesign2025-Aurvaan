4373597c010ecc042d0a482fbc29aba676a3a48c
John7-web <samraaurvaan@gmail.com>
2025-09-24 11:31:54 -0700

    Add files via upload

---
+++ public/webdesign2025-Aurvaan/public/script.js
@@ -0,0 +1,7 @@
+// Get a reference to the button element
+const button = document.getElementById('myButton');
+// Add an event listener for the 'click' event
+button.addEventListener('click', function() {
+  // Display an alert message when the button is clicked
+  alert('kanye');
+});
